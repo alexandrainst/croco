6365090295169cee9fe176ff981837760abd634d
Dan Saattrup Smart <dan.smart@alexandra.dk>
2026-06-26 10:04:53 +0200

    Initial commit

---
+++ src/croco/__init__.py
@@ -0,0 +1,1 @@
+"""Experiments with the CroCo post-training method."""
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Test suite for the croco package."""
+
+
+def test_dummy() -> None:
+    """Dummy test function, to ensure that pytest works."""
+    assert True
